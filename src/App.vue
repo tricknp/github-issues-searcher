78ee7a32fceb67c8326618cb7d5e5fd22c84e03f
Patrick <np_patrick@yahoo.com>
2018-03-11 19:19:10 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  
+  <div id="app">
+
+    <header class="head">
+        <h1>GitHub Issues Search</h1>
+    </header>
+
+    <router-view/>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
---
+++ src/components/GitHubIssues.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        
+        <form action="" class="form">
+            <div class="square">
+                <div class="inputs">
+                    <input type="text" id="" placeholder="github username">
+                    <input type="text" id="" placeholder="github repository">
+                </div>
+
+                <div class="buttons">
+                    <button>Search</button>
+                    <button>Clear</button>
+                </div>
+            </div>    
+        </form>   
+
+        <table class="">
+            <thead>
+                <tr>
+                    <th width="100">Número</th>
+                    <th>Título</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td class="text-center" colspan="2">Nenhuma issues encontrada!</td>
+                </tr>
+            </tbody>
+        </table>
+               
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'GitHubIssues',
+
+  data(){
+      return{
+
+      };
+  },
+
+  methods: {},
+
+}
+</script>
